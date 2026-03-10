7234be9d14bc653309c15b12719d806c42a7e355
mirkostanimirovic <mirkostanimirovic3@gmail.com>
2026-03-10 12:24:37 +0100

    Added cart functionality

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #main.py
 from product import Product
 from product_manager import ProductManager
-
+from cart import Cart
+import random
 
 manager = ProductManager()
 
@@ -18,3 +19,14 @@ print("Products:")
 manager.display_products()
 
 manager.total_inventory_value()
+
+cart = Cart()
+
+random_products = random.sample(manager.products, 3)
+
+for p in random_products:
+    cart.add_to_cart(p)
+
+cart.display_cart()
+
+print("Total price:", cart.total_price())
